04c21ee58006df968176114a976a6cb3ff47e64c
mariano.barcia <mariano.barcia@gmail.com>
2022-10-27 03:22:55 +0100

    Lean repos

--- src/main/java/com/example/poc/repository/PaymentRecordRepository.java
+++ src/main/java/com/example/poc/repository/PaymentRecordRepository.java
@@ -1,14 +1,18 @@
 package com.example.poc.repository;
 
+import com.example.poc.domain.CsvPaymentsFile;
 import com.example.poc.domain.PaymentRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface PaymentRecordRepository extends JpaRepository<PaymentRecord, Long> {
-
-    @Override
-    Optional<PaymentRecord> findById(Long aLong);
+    @Query("select r, ack, pst from PaymentRecord r" +
+            " inner join fetch r.ackPaymentSent ack" +
+            " left join fetch ack.paymentStatus pst" +
+            " where r.csvPaymentsFile = ?1")
+    List<PaymentRecord> findAllByFile(CsvPaymentsFile csvPaymentsFile);
 }
--- src/main/java/com/example/poc/repository/PaymentStatusRepository.java
+++ src/main/java/com/example/poc/repository/PaymentStatusRepository.java
@@ -4,10 +4,6 @@ import com.example.poc.domain.PaymentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PaymentStatusRepository extends JpaRepository<PaymentStatus, Long> {
-    @Override
-    Optional<PaymentStatus> findById(Long id);
 }

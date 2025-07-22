4ff3b0a7b8a93eced225b9f0f619e06e4dfe8737
mariano.barcia <mariano.barcia@gmail.com>
2025-07-22 23:28:06 +0200

    Reactive: input-csv-file-processing-svc - command

--- input-csv-file-processing-svc/src/main/java/com/example/poc/command/ProcessCsvPaymentsInputFileCommand.java
+++ input-csv-file-processing-svc/src/main/java/com/example/poc/command/ProcessCsvPaymentsInputFileCommand.java
@@ -1,37 +1,56 @@
 package com.example.poc.command;
 
-import com.example.poc.common.command.Command;
+import com.example.poc.common.command.ReactiveStreamingCommand;
 import com.example.poc.common.domain.CsvPaymentsInputFile;
 import com.example.poc.common.domain.PaymentRecord;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import io.smallrye.mutiny.Multi;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.stream.Stream;
+import java.util.concurrent.Executor;
 
 @ApplicationScoped
-public class ProcessCsvPaymentsInputFileCommand implements Command<CsvPaymentsInputFile, Stream<PaymentRecord>> {
+public class ProcessCsvPaymentsInputFileCommand implements ReactiveStreamingCommand<CsvPaymentsInputFile, PaymentRecord> {
+
+    private Executor executor;
+
+    @Inject
+    public ProcessCsvPaymentsInputFileCommand(@Named("virtualExecutor") Executor executor) {
+        this.executor = executor;
+    }
+
     @Override
-    public Stream<PaymentRecord> execute(CsvPaymentsInputFile csvFile) {
+    public Multi<PaymentRecord> execute(CsvPaymentsInputFile csvFile) {
         Logger logger = LoggerFactory.getLogger(getClass());
 
         try {
             CsvToBean<PaymentRecord> csvReader = new CsvToBeanBuilder<PaymentRecord>(new BufferedReader(new FileReader(csvFile.getFilepath())))
-                    .withType(PaymentRecord.class)
-                    .withSeparator(',')
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .withIgnoreEmptyLine(true)
-                    .build();
+                .withType(PaymentRecord.class)
+                .withSeparator(',')
+                .withIgnoreLeadingWhiteSpace(true)
+                .withIgnoreEmptyLine(true)
+                .build();
 
-            return csvReader.parse().stream()
-                    .map(record -> record.assignInputFile(csvFile));
+            return Multi.createFrom().items(() ->
+                csvReader.parse().stream()
+                    .map(record -> record.assignInputFile(csvFile))
+            ).runSubscriptionOn(executor);
+
         } catch (Exception e) {
             logger.error(e.getLocalizedMessage());
             throw new RuntimeException(e);
         }
+    }
+
+    // for test use
+    void setExecutor(Executor executor) {
+        this.executor = executor;
     }
 }

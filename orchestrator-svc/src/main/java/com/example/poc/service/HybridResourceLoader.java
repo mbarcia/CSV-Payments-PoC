8e1345af90d4573a2fe9549f2aec7fc1e398fcef
mariano.barcia <mariano.barcia@gmail.com>
2025-07-22 23:38:21 +0200

    Reactive: orchestrator-svc - shorter HybridResourceLoader.java

--- orchestrator-svc/src/main/java/com/example/poc/service/HybridResourceLoader.java
+++ orchestrator-svc/src/main/java/com/example/poc/service/HybridResourceLoader.java
@@ -8,11 +8,6 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 @ApplicationScoped
 public class HybridResourceLoader implements ResourceLoader {
@@ -60,100 +55,6 @@ public class HybridResourceLoader implements ResourceLoader {
         } catch (MalformedURLException e) {
             LOG.error("Error creating URL", e);
             return null;
-        }
-    }
-
-    /**
-     * Lists all resources in a directory, combining external files and JAR resources.
-     * External files take precedence if filenames conflict.
-     *
-     * @param directory Directory to list
-     * @return List of resource URLs
-     */
-    public List<URL> listResources(String directory) {
-        LOG.debug("Listing resources in: {}", directory);
-        Map<String, URL> resourceMap = new HashMap<>(); // Use map to handle duplicates
-
-        try {
-            // 1. First list external files
-            File externalDir = resolveExternalPath(directory);
-            if (externalDir.exists() && externalDir.isDirectory()) {
-                LOG.debug("Listing external files in: {}", externalDir);
-                File[] files = externalDir.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        if (file.isFile()) {
-                            LOG.debug("Found external file: {}", file.getName());
-                            resourceMap.put(file.getName(), file.toURI().toURL());
-                        }
-                    }
-                }
-            } else {
-                LOG.debug("External directory not found: {}", externalDir);
-            }
-
-            // 2. Then list classpath resources
-            String normalizedDir = directory.startsWith("/") ? directory.substring(1) : directory;
-            if (!normalizedDir.endsWith("/")) {
-                normalizedDir += "/";
-            }
-
-            // Try to list resources in the classpath
-            try {
-                URL dirUrl = Thread.currentThread().getContextClassLoader().getResource(normalizedDir);
-                if (dirUrl != null) {
-                    // Handle directory in filesystem
-                    if ("file".equals(dirUrl.getProtocol())) {
-                        File dir = new File(dirUrl.toURI());
-                        File[] files = dir.listFiles();
-                        if (files != null) {
-                            for (File file : files) {
-                                if (file.isFile() && !resourceMap.containsKey(file.getName())) {
-                                    LOG.debug("Found classpath file: {}", file.getName());
-                                    resourceMap.put(file.getName(), file.toURI().toURL());
-                                }
-                            }
-                        }
-                    }
-                    // Handle directory in JAR
-                    else if ("jar".equals(dirUrl.getProtocol())) {
-                        String jarPath = dirUrl.getPath();
-                        String jarFilePath = jarPath.substring(5, jarPath.indexOf("!"));
-                        String dirPath = jarPath.substring(jarPath.indexOf("!") + 2);
-
-                        try (JarFile jarFile = new JarFile(URLDecoder.decode(jarFilePath, StandardCharsets.UTF_8))) {
-                            Enumeration<JarEntry> entries = jarFile.entries();
-                            while (entries.hasMoreElements()) {
-                                JarEntry entry = entries.nextElement();
-                                if (!entry.isDirectory() && entry.getName().startsWith(dirPath)) {
-                                    String fileName = entry.getName().substring(dirPath.length());
-                                    if (!fileName.contains("/") && !resourceMap.containsKey(fileName)) {
-                                        URL url = Thread.currentThread().getContextClassLoader()
-                                                .getResource(entry.getName());
-                                        if (url != null) {
-                                            LOG.debug("Found JAR resource: {}", fileName);
-                                            resourceMap.put(fileName, url);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    LOG.debug("Directory URL not found in classpath: {}", normalizedDir);
-                }
-            } catch (Exception e) {
-                LOG.warn("Error listing classpath resources", e);
-            }
-
-            // Convert map to list
-            List<URL> result = new ArrayList<>(resourceMap.values());
-            LOG.debug("Total resources found: {}", result.size());
-            return result;
-
-        } catch (Exception e) {
-            LOG.error("Error listing resources", e);
-            return Collections.emptyList();
         }
     }
 

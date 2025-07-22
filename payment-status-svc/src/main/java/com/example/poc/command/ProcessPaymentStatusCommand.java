33a96508e33df05e1619919154e37e1f51dd1426
mariano.barcia <mariano.barcia@gmail.com>
2025-07-22 23:33:49 +0200

    Reactive: payment-status-svc - command

--- payment-status-svc/src/main/java/com/example/poc/command/ProcessPaymentStatusCommand.java
+++ payment-status-svc/src/main/java/com/example/poc/command/ProcessPaymentStatusCommand.java
@@ -1,27 +1,28 @@
 package com.example.poc.command;
 
-import com.example.poc.common.command.Command;
+import com.example.poc.common.command.ReactiveCommand;
 import com.example.poc.common.domain.PaymentOutput;
 import com.example.poc.common.domain.PaymentRecord;
 import com.example.poc.common.domain.PaymentStatus;
+import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class ProcessPaymentStatusCommand implements Command<PaymentStatus, PaymentOutput> {
+public class ProcessPaymentStatusCommand implements ReactiveCommand<PaymentStatus, PaymentOutput> {
     @Override
-    public PaymentOutput execute(PaymentStatus paymentStatus) {
+    public Uni<PaymentOutput> execute(PaymentStatus paymentStatus) {
         PaymentRecord paymentRecord = paymentStatus.getAckPaymentSent().getPaymentRecord();
 
-        return new PaymentOutput(
-                paymentStatus,
-                paymentRecord.getCsvId(),
-                paymentRecord.getRecipient(),
-                paymentRecord.getAmount(),
-                paymentRecord.getCurrency(),
-                paymentStatus.getAckPaymentSent().getConversationId(),
-                paymentStatus.getAckPaymentSent().getStatus(),
-                paymentStatus.getMessage(),
-                paymentStatus.getFee()
-        );
+        return Uni.createFrom().item(new PaymentOutput(
+            paymentStatus,
+            paymentRecord.getCsvId(),
+            paymentRecord.getRecipient(),
+            paymentRecord.getAmount(),
+            paymentRecord.getCurrency(),
+            paymentStatus.getAckPaymentSent().getConversationId(),
+            paymentStatus.getAckPaymentSent().getStatus(),
+            paymentStatus.getMessage(),
+            paymentStatus.getFee()
+        ));
     }
 }

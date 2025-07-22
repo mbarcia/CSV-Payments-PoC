cf8ffb1ca8e571f5e3c41d1a5e4a14c521c227ba
mariano.barcia <mariano.barcia@gmail.com>
2025-07-22 23:20:42 +0200

    Reactive: common - new gRPC reactive adapters

--- common/src/main/java/com/example/poc/common/service/GrpcServiceClientStreamingAdapter.java
+++ common/src/main/java/com/example/poc/common/service/GrpcServiceClientStreamingAdapter.java
@@ -1,28 +1,24 @@
 package com.example.poc.common.service;
 
+import io.grpc.Metadata;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
-import java.util.List;
-
 public abstract class GrpcServiceClientStreamingAdapter<GrpcIn, GrpcOut, DomainIn, DomainOut> {
 
-    protected abstract Service<List<DomainIn>, DomainOut> getService();
+    protected abstract ReactiveStreamingClientService<DomainIn, DomainOut> getService();
 
     protected abstract DomainIn fromGrpc(GrpcIn grpcIn);
 
     protected abstract GrpcOut toGrpc(DomainOut domainOut);
 
     public Uni<GrpcOut> remoteProcess(Multi<GrpcIn> requestStream) {
-        return requestStream
-            .onItem().transform(this::fromGrpc)
-            .collect().asList()
-            .onItem().transformToUni(inputList ->
-                VirtualThreadRunner.runOnVirtualThread(() -> getService().process(inputList))
-                    .onItem().transform(this::toGrpc)
-                    .onFailure().transform(throwable ->
-                        new RuntimeException("Processing failed: " + throwable.getMessage(), throwable)
-                    )
-            );
+
+        return getService()
+            .process(requestStream.onItem().transform(this::fromGrpc)) // Multi<DomainIn> → Uni<DomainOut>
+            .onItem().transform(this::toGrpc)                          // Uni<GrpcOut>
+            .onFailure().transform(new throwStatusRuntimeExceptionFunction());
     }
 }
--- common/src/main/java/com/example/poc/common/service/GrpcServiceStreamingAdapter.java
+++ common/src/main/java/com/example/poc/common/service/GrpcServiceStreamingAdapter.java
@@ -2,25 +2,19 @@ package com.example.poc.common.service;
 
 import io.smallrye.mutiny.Multi;
 
-import java.util.stream.Stream;
-
 public abstract class GrpcServiceStreamingAdapter<GrpcIn, GrpcOut, DomainIn, DomainOut> {
 
-    protected abstract Service<DomainIn, Stream<DomainOut>> getService();
+    protected abstract ReactiveStreamingService<DomainIn, DomainOut> getService();
 
     protected abstract DomainIn fromGrpc(GrpcIn grpcIn);
 
     protected abstract GrpcOut toGrpc(DomainOut domainOut);
 
     public Multi<GrpcOut> remoteProcess(GrpcIn grpcRequest) {
-        return VirtualThreadRunner.runOnVirtualThread(() -> {
-                DomainIn domainIn = fromGrpc(grpcRequest);
-                return getService().process(domainIn);
-            })
-            .onItem().transformToMulti(stream ->
-                Multi.createFrom().items(stream)
-                    .onTermination().invoke(stream::close)
-                    .onItem().transform(this::toGrpc)
-            );
+
+        return getService()
+            .process(fromGrpc(grpcRequest))   // Multi<DomainOut>
+            .onItem().transform(this::toGrpc) // Multi<GrpcOut>
+            .onFailure().transform(new throwStatusRuntimeExceptionFunction());
     }
 }

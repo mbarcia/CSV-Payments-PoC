c8bf6cdd777470b3f1755a8da41361b3481f541a
mariano.barcia <mariano.barcia@gmail.com>
2025-07-28 00:51:11 +0200

    Removed monolith version

--- src/main/java/com/example/poc/repository/CsvPaymentsOutputFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.poc.repository;
-
-import com.example.poc.domain.CsvPaymentsOutputFile;
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class CsvPaymentsOutputFileRepository implements PanacheRepository<CsvPaymentsOutputFile> {
-}

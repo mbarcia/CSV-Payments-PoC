597b4262ddc8112475dc12bffcecd2cdc635e2ae
mariano.barcia <mariano.barcia@gmail.com>
2025-07-27 23:24:05 +0200

    No more Commands (output-csv-file-processing-svc)

--- output-csv-file-processing-svc/src/main/java/com/example/poc/service/ProcessCsvPaymentsOutputFileReactiveService.java
+++ output-csv-file-processing-svc/src/main/java/com/example/poc/service/ProcessCsvPaymentsOutputFileReactiveService.java
@@ -1,23 +1,74 @@
+/*
+ * Copyright © 2023-2025 Mariano Barcia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.poc.service;
 
-import com.example.poc.command.ProcessPaymentOutputCommand;
-import com.example.poc.common.domain.CsvPaymentsOutputFile;
-import com.example.poc.common.domain.PaymentOutput;
-import com.example.poc.common.service.BaseReactiveStreamingClientService;
+import com.example.poc.common.domain.*;
+import com.example.poc.common.service.ReactiveStreamingClientService;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import lombok.Getter;
+import jakarta.inject.Named;
+import java.io.IOException;
+import java.util.concurrent.Executor;
 
 @ApplicationScoped
-@Getter
 public class ProcessCsvPaymentsOutputFileReactiveService
-    extends BaseReactiveStreamingClientService<PaymentOutput, CsvPaymentsOutputFile> {
-  @Inject ProcessPaymentOutputCommand command;
+    implements ReactiveStreamingClientService<PaymentOutput, CsvPaymentsOutputFile> {
+
+  @Inject
+  @Named("virtualExecutor")
+  Executor executor;
+
+  // for testing purposes only
+  public ProcessCsvPaymentsOutputFileReactiveService(Executor executor) {
+    this.executor = executor;
+  }
 
   @Override
-  public Uni<CsvPaymentsOutputFile> process(Multi<PaymentOutput> processableObj) {
-    return super.process(processableObj);
+  public Uni<CsvPaymentsOutputFile> process(Multi<PaymentOutput> paymentOutputList) {
+    return paymentOutputList
+        .collect()
+        .asList()
+        .onItem()
+        .transformToUni(
+            paymentOutputs ->
+                Uni.createFrom()
+                    .item(
+                        () -> {
+                          try (CsvPaymentsOutputFile file =
+                              this.getCsvPaymentsOutputFile(paymentOutputs.getFirst())) {
+                            file.getSbc().write(paymentOutputs);
+                            return file;
+                          } catch (Exception e) {
+                            throw new RuntimeException("Failed to write output file.", e);
+                          }
+                        })
+                    .runSubscriptionOn(executor));
+  }
+
+  protected CsvPaymentsOutputFile getCsvPaymentsOutputFile(PaymentOutput paymentOutput)
+      throws IOException {
+    assert paymentOutput != null;
+    PaymentStatus paymentStatus = paymentOutput.getPaymentStatus();
+    AckPaymentSent ackPaymentSent = paymentStatus.getAckPaymentSent();
+    PaymentRecord paymentRecord = ackPaymentSent.getPaymentRecord();
+    String csvPaymentsInputFilePath = paymentRecord.getCsvPaymentsInputFilePath();
+
+    return new CsvPaymentsOutputFile(csvPaymentsInputFilePath);
   }
 }
--- output-csv-file-processing-svc/src/test/java/com/example/poc/service/ProcessCsvPaymentsOutputFileReactiveServiceTest.java
+++ output-csv-file-processing-svc/src/test/java/com/example/poc/service/ProcessCsvPaymentsOutputFileReactiveServiceTest.java
@@ -1,62 +1,190 @@
+/*
+ * Copyright © 2023-2025 Mariano Barcia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.poc.service;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-import com.example.poc.command.ProcessPaymentOutputCommand;
-import com.example.poc.common.domain.CsvPaymentsOutputFile;
-import com.example.poc.common.domain.PaymentOutput;
+import com.example.poc.common.domain.*;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.helpers.test.UniAssertSubscriber;
+import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Currency;
+import java.util.List;
 import java.util.UUID;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.io.TempDir;
 
-@SuppressWarnings("unchecked")
 class ProcessCsvPaymentsOutputFileReactiveServiceTest {
 
-  @InjectMocks ProcessCsvPaymentsOutputFileReactiveService service;
+  ProcessCsvPaymentsOutputFileReactiveService service;
 
-  @Mock ProcessPaymentOutputCommand command;
+  @TempDir static File tempFile;
+
+  CsvPaymentsInputFile csvPaymentsInputFile;
 
   @BeforeEach
   void setUp() {
-    MockitoAnnotations.openMocks(this);
+    service = new ProcessCsvPaymentsOutputFileReactiveService(Runnable::run);
+  }
+
+  @AfterEach
+  void tearDown() throws IOException {
+    Files.deleteIfExists(tempFile.toPath());
   }
 
   @Test
-  void process() throws IOException {
+  void execute_happy() throws IOException {
     // Given
-    PaymentOutput paymentOutput =
-        new PaymentOutput(
-            new com.example.poc.common.domain.PaymentStatus(),
-            UUID.randomUUID().toString(),
-            "John Doe",
+    Multi<PaymentOutput> paymentOutputList = getMultiPaymentOutput();
+
+    Uni<CsvPaymentsOutputFile> resultUni = service.process(paymentOutputList);
+
+    // Inspect/verify the output file
+    // When: Read the first two lines
+    List<String> lines =
+        Files.readAllLines(Path.of(resultUni.await().indefinitely().getFilepath()));
+
+    // Then: Assert header and first record
+    assertThat(lines).hasSizeGreaterThanOrEqualTo(2);
+
+    String headerLine = lines.get(0);
+    String firstRecordLine = lines.get(1);
+    String secondRecordLine = lines.get(2);
+
+    AssertionsForClassTypes.assertThat(headerLine)
+        .contains(
+            "AMOUNT", "CSV ID", "CURRENCY", "FEE", "MESSAGE", "RECIPIENT", "REFERENCE", "STATUS");
+    AssertionsForClassTypes.assertThat(firstRecordLine)
+        .contains(
+            "100.00",
+            "80e055c9-7dbe-4ef0-ad37-8360eb8d1e3e",
+            "USD",
+            "",
+            "",
+            "recipient123",
+            "abacd5c7-2230-4a24-a665-32a542468ea5",
+            "");
+    AssertionsForClassTypes.assertThat(secondRecordLine)
+        .contains(
+            "450.01",
+            "2d8acc5b-8dae-4240-b37c-893318aba63f",
+            "GBP",
+            "",
+            "",
+            "234recipient",
+            "746ab623-c070-49dd-87fb-ed2f39f2f3cf",
+            "");
+  }
+
+  private Multi<PaymentOutput> getMultiPaymentOutput() {
+    // Given
+    csvPaymentsInputFile = new CsvPaymentsInputFile(tempFile);
+    PaymentRecord paymentRecord =
+        new PaymentRecord(
+            "80e055c9-7dbe-4ef0-ad37-8360eb8d1e3e",
+            "recipient123",
             new BigDecimal("100.00"),
-            Currency.getInstance("USD"),
-            UUID.randomUUID(),
-            1L,
-            "Success",
-            new BigDecimal("1.50"));
-    Multi<PaymentOutput> paymentOutputMulti = Multi.createFrom().item(paymentOutput);
+            Currency.getInstance("USD"));
+    paymentRecord.assignInputFile(csvPaymentsInputFile);
+    PaymentOutput paymentOutput = getPaymentOutput(paymentRecord);
+    paymentOutput.setConversationId(UUID.fromString("abacd5c7-2230-4a24-a665-32a542468ea5"));
 
-    CsvPaymentsOutputFile expectedOutputFile = new CsvPaymentsOutputFile("/tmp/output.csv");
+    PaymentRecord paymentRecord2 =
+        new PaymentRecord(
+            "2d8acc5b-8dae-4240-b37c-893318aba63f",
+            "234recipient",
+            new BigDecimal("450.01"),
+            Currency.getInstance("GBP"));
+    paymentRecord2.assignInputFile(csvPaymentsInputFile);
+    PaymentOutput paymentOutput2 = getPaymentOutput(paymentRecord2);
+    paymentOutput2.setConversationId(UUID.fromString("746ab623-c070-49dd-87fb-ed2f39f2f3cf"));
 
-    // When
-    when(command.execute(any(Multi.class))).thenReturn(Uni.createFrom().item(expectedOutputFile));
+    return Multi.createFrom().items(paymentOutput, paymentOutput2);
+  }
 
-    Uni<CsvPaymentsOutputFile> resultUni = service.process(paymentOutputMulti);
+  private Multi<PaymentOutput> getBadMultiPaymentOutput() {
+    // Given
+    PaymentRecord paymentRecord =
+        new PaymentRecord(
+            "80e055c9-7dbe-4ef0-ad37-8360eb8d1e3e",
+            "recipient123",
+            new BigDecimal("100.00"),
+            Currency.getInstance("USD"));
+    paymentRecord.setCsvPaymentsInputFilePath("bad-file-path");
+    PaymentOutput paymentOutput = getPaymentOutput(paymentRecord);
+    paymentOutput.setConversationId(UUID.fromString("abacd5c7-2230-4a24-a665-32a542468ea5"));
+
+    csvPaymentsInputFile = new CsvPaymentsInputFile(tempFile);
+    PaymentRecord paymentRecord2 =
+        new PaymentRecord(
+            "2d8acc5b-8dae-4240-b37c-893318aba63f",
+            "234recipient",
+            new BigDecimal("450.01"),
+            Currency.getInstance("GBP"));
+    paymentRecord2.assignInputFile(csvPaymentsInputFile);
+    PaymentOutput paymentOutput2 = getPaymentOutput(paymentRecord2);
+    paymentOutput2.setConversationId(UUID.fromString("746ab623-c070-49dd-87fb-ed2f39f2f3cf"));
+
+    return Multi.createFrom().items(paymentOutput, paymentOutput2);
+  }
+
+  private PaymentOutput getPaymentOutput(PaymentRecord paymentRecord) {
+    AckPaymentSent ackPaymentSent = new AckPaymentSent();
+    ackPaymentSent.setPaymentRecord(paymentRecord);
+    ackPaymentSent.setPaymentRecordId(paymentRecord.getId());
+
+    PaymentStatus paymentStatus = new PaymentStatus();
+    paymentStatus.setAckPaymentSent(ackPaymentSent);
+    paymentStatus.setAckPaymentSentId(ackPaymentSent.getId());
+
+    return new PaymentOutput(
+        paymentStatus,
+        paymentRecord.getCsvId(),
+        paymentRecord.getRecipient(),
+        paymentRecord.getAmount(),
+        paymentRecord.getCurrency(),
+        ackPaymentSent.getConversationId(),
+        ackPaymentSent.getStatus(),
+        paymentStatus.getMessage(),
+        paymentStatus.getFee());
+  }
+
+  @Test
+  void execute_unhappy() {
+    // Given
+    Multi<PaymentOutput> paymentOutputList = getBadMultiPaymentOutput();
+
+    // When (mock the getCsvPaymentsOutputFile method within the command)
+
+    Uni<CsvPaymentsOutputFile> resultUni = service.process(paymentOutputList);
 
     // Then
     UniAssertSubscriber<CsvPaymentsOutputFile> subscriber =
         resultUni.subscribe().withSubscriber(UniAssertSubscriber.create());
-    subscriber.awaitItem().assertItem(expectedOutputFile);
+    subscriber
+        .awaitFailure()
+        .assertFailedWith(RuntimeException.class, "Failed to write output file.");
   }
 }

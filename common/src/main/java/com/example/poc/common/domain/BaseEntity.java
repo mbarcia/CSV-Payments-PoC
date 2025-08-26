6cd28d3fff6b6deb67efed9d84336de1df903c14
mariano.barcia <mariano.barcia@gmail.com>
2025-08-26 18:23:37 +0200

    common: removed Hibernate/PG dependency from BaseEntity

--- common/src/main/java/com/example/poc/common/domain/BaseEntity.java
+++ common/src/main/java/com/example/poc/common/domain/BaseEntity.java
@@ -16,11 +16,8 @@
 
 package com.example.poc.common.domain;
 
-import static io.quarkus.hibernate.reactive.panache.Panache.withTransaction;
 
 import com.opencsv.bean.CsvIgnore;
-import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
-import io.smallrye.mutiny.Uni;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
@@ -31,7 +28,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @MappedSuperclass
-public abstract class BaseEntity extends PanacheEntityBase {
+public abstract class BaseEntity {
 
   @Id
   @CsvIgnore
@@ -40,9 +37,5 @@ public abstract class BaseEntity extends PanacheEntityBase {
 
   public BaseEntity() {
     id = UUID.randomUUID();
-  }
-
-  public Uni<BaseEntity> save() {
-    return withTransaction(this::persist);
   }
 }

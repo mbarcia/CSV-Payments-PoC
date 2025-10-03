112722e3744b94787941509a28f458a13409e59a
mariano.barcia <mariano.barcia@gmail.com>
2025-10-03 19:22:23 +0200

    StepConfigProvider: not sure about this

--- pipeline-framework/runtime/src/main/java/io/github/mbarcia/pipeline/annotation/StepConfigProvider.java
+++ pipeline-framework/runtime/src/main/java/io/github/mbarcia/pipeline/annotation/StepConfigProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2023-2025 Mariano Barcia
+ * Copyright (c) 2023-2025 Mariano Barcia
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package io.github.mbarcia.pipeline.annotation;
 
 import io.github.mbarcia.pipeline.config.StepConfig;
 import io.github.mbarcia.pipeline.step.ConfigurableStep;
-import jakarta.enterprise.inject.spi.CDI;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -37,12 +37,20 @@ public class StepConfigProvider {
      */
     public static StepConfig getStepConfig(Class<? extends ConfigurableStep> stepClass) {
         return configCache.computeIfAbsent(stepClass, cls -> {
-            try {
-                // Try to get the step instance from CDI
-                ConfigurableStep step = CDI.current().select(stepClass).get();
-                return step.effectiveConfig();
-            } catch (Exception e) {
-                // If we can't get the step from CDI, return a default config
+            // First, try to get the configuration from the annotation
+            PipelineStep annotation = cls.getAnnotation(PipelineStep.class);
+            
+            if (annotation != null) {
+                // Build configuration from the annotation
+                StepConfig config = new StepConfig();
+                config.autoPersist(annotation.autoPersist())
+                       .debug(annotation.debug())
+                       .recoverOnFailure(annotation.recoverOnFailure())
+                       .backpressureBufferCapacity(annotation.backpressureBufferCapacity())
+                       .backpressureStrategy(annotation.backpressureStrategy());
+                return config;
+            } else {
+                // If no annotation, return a default config
                 return new StepConfig();
             }
         });

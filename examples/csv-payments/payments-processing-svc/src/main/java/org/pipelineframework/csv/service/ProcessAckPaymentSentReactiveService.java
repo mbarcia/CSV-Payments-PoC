aa4ec94773ee631750963f29d1f95939e275639c
mariano.barcia <mariano.barcia@gmail.com>
2025-11-17 13:26:08 +0100

    Redundant @Inject
    Signed-off-by: mariano.barcia <mariano.barcia@gmail.com>

--- examples/csv-payments/payments-processing-svc/src/main/java/org/pipelineframework/csv/service/ProcessAckPaymentSentReactiveService.java
+++ examples/csv-payments/payments-processing-svc/src/main/java/org/pipelineframework/csv/service/ProcessAckPaymentSentReactiveService.java
@@ -52,7 +52,6 @@ public class ProcessAckPaymentSentReactiveService
     
   private static final Logger LOG = Logger.getLogger(ProcessAckPaymentSentReactiveService.class);
 
-  @Inject
   PollAckPaymentSentService pollAckPaymentSentService;
 
   @Inject

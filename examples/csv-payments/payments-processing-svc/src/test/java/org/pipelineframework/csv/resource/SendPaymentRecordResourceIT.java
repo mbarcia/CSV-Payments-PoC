1af9e34e08b5dfc1adb5c30586c058c144abdad4
mariano.barcia <mariano.barcia@gmail.com>
2025-11-25 17:21:31 +0100

    Disabling test since restEnabled is now false
    Signed-off-by: mariano.barcia <mariano.barcia@gmail.com>

--- examples/csv-payments/payments-processing-svc/src/test/java/org/pipelineframework/csv/resource/SendPaymentRecordResourceIT.java
+++ examples/csv-payments/payments-processing-svc/src/test/java/org/pipelineframework/csv/resource/SendPaymentRecordResourceIT.java
@@ -26,9 +26,11 @@ import io.restassured.http.ContentType;
 import java.util.UUID;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
+@Disabled
 class SendPaymentRecordResourceIT {
 
     @BeforeAll
